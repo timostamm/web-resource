94ff30cfc8319e045def229775762731191da691
Timo Stamm <ts@timostamm.de>
2020-02-07 02:43:35 +0100

    Please symfony

--- tests/HashStorageTest.php
+++ tests/HashStorageTest.php
@@ -3,81 +3,81 @@
 namespace TS\Web\Resource;
 
 
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 use TS\Web\Resource\Exception\IOException;
-use org\bovigo\vfs\vfsStream;
 
 
 class HashStorageTest extends TestCase
 {
 
-	
-	public function testEmpty()
-	{
-		$this->assertCount(0, $this->storage->listHashes());
-	}
-	
-	public function testCleanupAfterWriteFailure()
-	{
-		$mock = $this->getMockBuilder(FileResource::class)
-			->disableArgumentCloning()
-			->disallowMockingUnknownTypes()
-			->setConstructorArgs([
-				$this->a->getPath()
-			])
-			->setMethods([
-				'getStream'
-			])
-			->getMock();
-		
-		$mock->method('getStream')->willThrowException(new \Exception('failure'));
-		
-		try {
-			
-			$this->storage->put($mock);
-			$this->fail('Expected exception.');
-			
-		} catch (IOException $ex) {
-			$this->assertEquals('Failed to store resource.', $ex->getMessage());
-			$this->assertCount(0, $this->storage->listHashes());
-		}
-	}
 
-	public function testHas()
-	{
-		$this->assertFalse($this->storage->has($this->a->getHash()));
-		$this->storage->put($this->a);
-		$this->assertTrue($this->storage->has($this->a->getHash()));
-	}
+    public function testEmpty()
+    {
+        $this->assertCount(0, $this->storage->listHashes());
+    }
 
-	public function testPut()
-	{
-		$this->assertCount(0, $this->storage->listHashes());
-		$this->storage->put($this->a);
-		$this->assertCount(1, $this->storage->listHashes());
-	}
+    public function testCleanupAfterWriteFailure()
+    {
+        $mock = $this->getMockBuilder(FileResource::class)
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->setConstructorArgs([
+                $this->a->getPath()
+            ])
+            ->setMethods([
+                'getStream'
+            ])
+            ->getMock();
 
-	private $storageDir;
+        $mock->method('getStream')->willThrowException(new \Exception('failure'));
 
-	private $storage;
+        try {
 
-	private $a;
+            $this->storage->put($mock);
+            $this->fail('Expected exception.');
 
-	private $b;
+        } catch (IOException $ex) {
+            $this->assertEquals('Failed to store resource.', $ex->getMessage());
+            $this->assertCount(0, $this->storage->listHashes());
+        }
+    }
 
-	public function setUp()
-	{
-		
-		$root = vfsStream::setup('root', null, [
-			'a.txt' => 'a content',
-			'b.txt' => 'b content'
-		]);
-		
-		$this->a = Resource::fromFile($root->url() . '/a.txt');
-		$this->a = Resource::fromFile($root->url() . '/b.txt');
-		$this->storageDir = $root->url() . '/store';
-		$this->storage = new HashStorage($this->storageDir);
-	}
+    public function testHas()
+    {
+        $this->assertFalse($this->storage->has($this->a->getHash()));
+        $this->storage->put($this->a);
+        $this->assertTrue($this->storage->has($this->a->getHash()));
+    }
+
+    public function testPut()
+    {
+        $this->assertCount(0, $this->storage->listHashes());
+        $this->storage->put($this->a);
+        $this->assertCount(1, $this->storage->listHashes());
+    }
+
+    private $storageDir;
+
+    private $storage;
+
+    private $a;
+
+    private $b;
+
+    public function setUp(): void
+    {
+
+        $root = vfsStream::setup('root', null, [
+            'a.txt' => 'a content',
+            'b.txt' => 'b content'
+        ]);
+
+        $this->a = Resource::fromFile($root->url() . '/a.txt');
+        $this->a = Resource::fromFile($root->url() . '/b.txt');
+        $this->storageDir = $root->url() . '/store';
+        $this->storage = new HashStorage($this->storageDir);
+    }
 
 }
 
--- tests/WebTestCase.php
+++ tests/WebTestCase.php
@@ -4,20 +4,20 @@ namespace TS\Web\Resource;
 
 
 if (php_sapi_name() == 'cli-server') {
-	if (preg_match('/^\/$/', $_SERVER["REQUEST_URI"])) {
-		header('Content-Type: text/html');
-		print "index";
-	} else if (preg_match('/^\/foo-no-content-length$/', $_SERVER["REQUEST_URI"])) {
-		header('Content-Type: application/x-foo');
-		print "foo";
-	} else if (preg_match('/^\/foo-error$/', $_SERVER["REQUEST_URI"])) {
-		header('HTTP/1.1 500 Internal Server Error');
-		print "foo";
-	} else {
-		// return false to route static assets
-		return false;
-	}
-	return true;
+    if (preg_match('/^\/$/', $_SERVER["REQUEST_URI"])) {
+        header('Content-Type: text/html');
+        print "index";
+    } else if (preg_match('/^\/foo-no-content-length$/', $_SERVER["REQUEST_URI"])) {
+        header('Content-Type: application/x-foo');
+        print "foo";
+    } else if (preg_match('/^\/foo-error$/', $_SERVER["REQUEST_URI"])) {
+        header('HTTP/1.1 500 Internal Server Error');
+        print "foo";
+    } else {
+        // return false to route static assets
+        return false;
+    }
+    return true;
 }
 
 
@@ -28,27 +28,28 @@ use Symfony\Component\Process\Process;
 abstract class WebTestCase extends TestCase
 {
 
-	public static $base_url;
+    public static $base_url;
 
-	private static $port = 8000;
+    private static $port = 8000;
 
-	private static $webserver;
+    /** @var Process */
+    private static $webserver;
 
-	public static function setUpBeforeClass()
-	{
-		self::$webserver = new Process('exec php -S localhost:' . self::$port . ' -t ' . __DIR__ . '/Data ' . __FILE__);
-		self::$base_url = 'http://localhost:' . self::$port . '/';
-		self::$port ++;
-		self::$webserver->start();
-		usleep(100 * 1000);
-	}
+    public static function setUpBeforeClass(): void
+    {
+        self::$webserver = new Process('exec php -S localhost:' . self::$port . ' -t ' . __DIR__ . '/Data ' . __FILE__);
+        self::$base_url = 'http://localhost:' . self::$port . '/';
+        self::$port++;
+        self::$webserver->start();
+        usleep(100 * 1000);
+    }
 
-	public static function tearDownAfterClass()
-	{
-		if (!self::$webserver->isRunning()) {
-			throw new \Exception('Webserver seems to have failed. ' . self::$webserver->getErrorOutput() );
-		}
-		self::$webserver->stop();
-	}
+    public static function tearDownAfterClass(): void
+    {
+        if (!self::$webserver->isRunning()) {
+            throw new \Exception('Webserver seems to have failed. ' . self::$webserver->getErrorOutput());
+        }
+        self::$webserver->stop();
+    }
 
 }
